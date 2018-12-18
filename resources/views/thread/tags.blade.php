17eab39eb5cf871f499cb4898c8fb9d812de8b97
rikkonem <michalkostyra3@gmail.com>
2018-12-18 13:50:16 +0100

    Delete unnecessary comments

--- resources/views/thread/tags.blade.php
+++ resources/views/thread/tags.blade.php
@@ -3,18 +3,13 @@
     @if($tags->count())
         <form method="get" action="{{url('/threads-with-tags')}}">
             Tags:
-
             @foreach($tags as $tag)
                 @if(!empty($selectedTags))
                         <input type="checkbox" name="tags[]" value="{{$tag->slug}}" onchange="this.form.submit()"
                         {{ in_array($tag->slug, $selectedTags) ? "checked" : null}}/>{{$tag->slug}}
-
                 @else
                     <input type="checkbox" name="tags[]" value="{{$tag->slug}}" onchange="this.form.submit()"/>{{$tag->slug}}
                 @endif
-{{--
-                <a href="{{url('threads-with-tag/' . $tag->slug)}}" class="badge badge-{{ ($tag->slug == (isset($selectedTag) ? $selectedTag : null )) ? "primary" : "light" }}">{{$tag->slug}}</a>
---}}
             @endforeach
         </form>
 
